0a33e4a8f1b846906ca8ca85a4c9f11d2e18b149
gabriel <ferre490@gmail.com>
2019-06-07 19:36:01 -0300

    Colocando o novo script pro banco de dados

--- VoceAluga/BancoDeTeste.sql
+++ VoceAluga/BancoDeTeste.sql
@@ -1,34 +1,47 @@
 -- executar como root
 create user 'testuser'@'localhost' identified by '123test'
-create database testdb;
-grant all privileges on testdb . * to 'testuser'@'localhost';
+create database bancoDeDados
+default character set utf8
+default collate utf8_general_ci;
 
-use testdb;
+use bancoDeDados;
 
-create table Customer(
-    Id int not null auto_increment primary key,
-    Cpf varchar(20) unique not null,
-    FirstName varchar(255) not null default '',
-    LastName varchar(255) not null default '',
-    Cnh varchar(20) not null default '',
-    Birthday date not null,
-    Cellphone varchar(20)
-);
+create table clientes (
+cpf varchar(11) unique not null,
+nome varchar(45) not null,
+endereco varchar(45) not null,
+telefone varchar(45),
+nascimento date not null,
+validadecnh date not null,
+primary key(cpf)
+)default charset utf8;
 
-create table Carro(
-	Id int not null auto_increment primary key,
-	Modelo varchar(40) not null default '',
-	Classe varchar(2) not null default '',
-	Idade int not null default 0,
-	Quilometragem float not null default 0
-);
+create table carros(
+placa varchar(7) not null unique,
+quilometragem int,
+grupo varchar(1),
+ano int,
+modelo varchar(20),
+primary key(placa)
+)default charset utf8;
 
-INSERT INTO `Customer` VALUES
-    (2,'22222222222','João','Mamão','5555555555','3216-03-03','22222222'),
-    (3,'11111111111','Joaquim José','da Silva Xavier','0000000000','1950-01-05','11111111'),
-    (4,'33333333333','José','das Couves','0101010101','9631-12-31','33333333333'),
-    (5,'44444444444','Nana','Shara','4444444444','0003-01-31','5521444444444');
+create table alocacoes(
+id int not null,
+idcliente varchar(11) not null,
+idcarro varchar(7) not null,
+problema varchar(50),
+primary key(id),
+foreign key(idcliente) references clientes(cpf),
+foreign key(idcarro) references carros(placa)
+)default charset utf8;
 
-INSERT INTO `Carro` VALUES
-    (1,'Uno','A','0', '0'),
-    (2, 'Brasilia','A', '123', '456');
+create table reservas(
+id int not null,
+idcliente varchar(11) not null,
+data date,
+grupo varchar(1),
+modelo varchar(20),
+duracao int,
+primary key(id),
+foreign key(idcliente) references clientes(cpf)
+)default charset utf8;
